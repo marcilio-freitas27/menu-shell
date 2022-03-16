dbce59dedd68b5f86d01eed5761f51d98fcdda82
Marcilio Freitas <marbass18@gmail.com>
2022-03-16 10:37:21 -0300

    Verificar o uso de input com vared ou read (zsh ou outros)

--- menu.sh
+++ menu.sh
@@ -14,7 +14,13 @@ EOF
 list=("teste" "novo" "velho");
 
 # entrada de dados. -p para fazer a entrada junto com uma variável(acho que é isso kk)
-read -p "Escolha a operação: " op;
+# se o shell atual for o zsh, utilize o vared ao invés do read para input de dados 
+if [ $0 = "zsh" ]
+then 
+	vared -p "Escolha a operação: " -c op;
+else
+	read -p "Escolha a operação: " op;
+fi
 
 # variáve que diz a quantidade de itens do array
 count=${#list[@]};
@@ -23,9 +29,16 @@ count=${#list[@]};
 
 # Adicionar: Pede um nome e insere ele na última posição do array
 adicionar(){
-	read -p "Informa o nome: " nome;
-	list[${count}+1]=${nome};
-	echo -e "${nome} foi adicionado.";
+	if [ $0 = "zsh" ]
+	then 
+		vared -p "Informa o nome: " -c nome;
+		list[${count}+1]=${nome};
+		echo -e "${nome} foi adicionado.";
+	else
+		read -p "Informa o nome: " nome;
+		list[${count}+1]=${nome};
+		echo -e "${nome} foi adicionado.";
+	fi
 }
 
 # Listar: Exibi todos os itens da lista(semelhante a count. o * faz o mesmo do @)
@@ -37,20 +50,38 @@ listar(){
 # Deletar: Mostra quantos itens tem na lista, Pergunta o index do item para excluir através da posição(unset)
 
 deletar(){
-	echo -e "A lista tem ${#list[@]} items.";
-	read -p "Qual item da lista vc quer apagar? " index;
-	unset list[${index}];
-	echo -e "O item foi apagado da sua lista.";
+	if [ $0 = "zsh" ]
+	then 
+		echo -e "A lista tem ${#list[@]} items.";
+		vared -p "Qual item da lista vc quer apagar? " -c index;
+		unset list[${index}];
+		echo -e "O item foi apagado da sua lista.";
+	else
+		echo -e "A lista tem ${#list[@]} items.";
+		read -p "Qual item da lista vc quer apagar? " index;
+		unset list[${index}];
+		echo -e "O item foi apagado da sua lista.";
+	fi
+	
 }
 
 # Atualizar: semelhante ao deletar para pegar o index e excluir através dele
 
 atualizar(){
-	echo -e "a lista tem ${#list[@]} items";
-	read -p "Qual o item que vc irá alterar?" item;
-	read -p "Insira o novo nome: " novoItem
-	list[${item}]=${novoItem};
-	echo -e "O item foi alterado.";
+	if [ $0 = "zsh" ]
+	then
+		echo -e "a lista tem ${#list[@]} items";
+		vared -p "Qual o item que vc irá alterar?" -c item;
+		read -p "Insira o novo nome: " novoItem
+		list[${item}]=${novoItem};
+		echo -e "O item foi alterado.";
+	else
+		echo -e "a lista tem ${#list[@]} items";
+		read -p "Qual o item que vc irá alterar?" item;
+		read -p "Insira o novo nome: " novoItem
+		list[${item}]=${novoItem};
+		echo -e "O item foi alterado.";
+	fi
 }
 
 # enquanto continuar for igual (= : comparação) mantem o loop
@@ -63,16 +94,30 @@ do
 	# case fecha com esac
 	if [ $op = "vazio" ]
 	then
-		read -p "Escolha a operacao; " op;
-		# semelhante ao switch case do js, java, etc. 
+		if [ $0 = "zsh" ]
+		then
+			vared -p "Escolha a operacao; " -c op;
+			# semelhante ao switch case do js, java, etc. 
 
-		case $op in
-			1) adicionar;;
-			2) listar;;
-			3) atualizar;;
-			4) deletar;;
-			*) echo -e "Entrada Inválida!";;
-		esac
+			case $op in
+				1) adicionar;;
+				2) listar;;
+				3) atualizar;;
+				4) deletar;;
+				*) echo -e "Entrada Inválida!";;
+			esac
+		else
+			read -p "Escolha a operacao; " op;
+			# semelhante ao switch case do js, java, etc. 
+
+			case $op in
+				1) adicionar;;
+				2) listar;;
+				3) atualizar;;
+				4) deletar;;
+				*) echo -e "Entrada Inválida!";;
+			esac
+		fi
 	else
 		case $op in 
 			1) adicionar;;
@@ -85,7 +130,13 @@ do
 	
 	# só sai do loop se vc escolher qualquer outra coisa diferente de sim
 	# Sai do programa
-	read -p "Deseja executar outra operação? (sim/nao)" continuar;
-	op="vazio";
+	if [ $0 = "zsh" ]
+	then
+		vared -p "Deseja executar outra operação? (sim/nao)" -c continuar;
+		op="vazio";
+	else
+		read -p "Deseja executar outra operação? (sim/nao)" continuar;
+		op="vazio";
+	fi
 
 done
---
+++ teste.sh
@@ -0,0 +1,7 @@
+# nome recebe testando no stdout
+# echo "testando" > nome.txt
+# nome substitui testando por alterando no stdout
+# echo "alterando" > nome.txt
+# nome recebe testando uma linha após alterando no stdout
+# echo "testando" >> nome.txt
+
